25ef17d898a7ff2c0672fa58f6d31a28fa75db4b
Alejo <alejandro.forero@miaguila.com>
2020-04-15 14:42:12 -0500

    feat: add testing pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,16 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () => import('./home/home.module').then(module => module.HomeModule)
+  },
+  {
+    path: 'categories/:slug',
+    loadChildren: () => import('./categories/categories.module').then(module => module.CategoriesModule)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes, {
